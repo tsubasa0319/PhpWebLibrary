d9604f5e907f80fe23f5133ae5dfa6c63afd3438
Tsubasa Kadowaki <mocchihouse_mocchi@hotmail.co.jp>
2025-01-09 18:23:59 +0900

    (2024/02/27 15:40:05)Smartyプラグインを導入、セレクトボックス/ラジオボタンリスト/吹き出しのスタイルを実装。

--- www/libs/php/imports/tsubasaLibs/loader.php
+++ www/libs/php/imports/tsubasaLibs/loader.php
@@ -4,7 +4,9 @@
 //
 // History:
 // 0.00.00 2024/01/23 作成。
+// 0.08.00 2024/02/27 Smartyプラグインを追加。
 // -------------------------------------------------------------------------------------------------
 require_once __DIR__ . '/database/DbConnectorBase.php';
 require_once __DIR__ . '/type/loader.php';
 require_once __DIR__ . '/web/Events.php';
+require_once __DIR__ . '/web/SmartyPlugins.php';

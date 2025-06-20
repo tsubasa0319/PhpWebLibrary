01e2f6015f7a7ad4095c8855cc8968ac6f316b2a
Tsubasa Kadowaki <mocchihouse_mocchi@hotmail.co.jp>
2025-06-20 11:11:21 +0900

    (2024/03/23 15:13:24)選択クエリ予定クラスを実装。

---
+++ www/libs/php/imports/tsubasaLibs/database/SelectPlan.php
@@ -0,0 +1,111 @@
+<?php
+// -------------------------------------------------------------------------------------------------
+// 選択クエリ予定クラス
+//
+// History:
+// 0.16.00 2024/03/23 作成。
+// -------------------------------------------------------------------------------------------------
+namespace tsubasaLibs\database;
+use Stringable;
+/**
+ * 選択クエリ予定クラス
+ * 
+ * @since 0.16.00
+ * @version 0.16.00
+ */
+class SelectPlan {
+    // ---------------------------------------------------------------------------------------------
+    // プロパティ
+    /** @var array 検索値リスト */
+    protected $values;
+    /** @var Record レコード */
+    protected $record;
+    /** @var bool 実行済かどうか */
+    public $isExecuted;
+    // ---------------------------------------------------------------------------------------------
+    // コンストラクタ/デストラクタ
+    public function __construct() {
+        $this->setInit();
+    }
+    // ---------------------------------------------------------------------------------------------
+    // メソッド
+    /**
+     * 重複した予定かどうか
+     * 
+     * @return bool 結果
+     */
+    public function isDuplicate($values): bool {
+        if (!is_array($values)) return false;
+        if (count($values) !== count($this->values)) return false;
+        for ($i = 0; $i < count($values); $i++) {
+            $val1 = $values[$i];
+            $val2 = $this->values[$i];
+            if ($val1 instanceof Stringable) $val1 = (string)$val1;
+            if ($val2 instanceof Stringable) $val2 = (string)$val2;
+            if ($val1 !== $val2)
+                return false;
+        }
+        return true;
+    }
+    /**
+     * 対象レコードかどうか
+     * 
+     * @return bool 結果
+     */
+    public function isTarget(Record $record): bool {
+        $values = $record->getIndexKeyValues();
+        if (count($values) < count($this->values)) return false;
+
+        for ($i = 0; $i < count($this->values); $i++) {
+            $val1 = $values[$i];
+            $val2 = $this->values[$i];
+            if ($val1 instanceof Stringable) $val1 = (string)$val1;
+            if ($val2 instanceof Stringable) $val2 = (string)$val2;
+            if ($val1 !== $val2)
+                return false;
+        }
+        return true;
+    }
+    /**
+     * 検索値リストを取得
+     * 
+     * @return array 検索値リスト
+     */
+    public function getValues(): array {
+        return $this->values;
+    }
+    /**
+     * 検索値リストを設定
+     * 
+     * @param array 検索値リスト
+     */
+    public function setValues(array $values) {
+        $this->values = $values;
+    }
+    /**
+     * レコードを取得
+     * 
+     * @return ?Record レコード
+     */
+    public function getRecord(): ?Record {
+        return $this->record;
+    }
+    /**
+     * レコードを設定
+     * 
+     * @param Record $record レコード
+     */
+    public function setRecord(Record $record) {
+        $this->record = $record;
+    }
+    // ---------------------------------------------------------------------------------------------
+    // 内部処理
+    /**
+     * 初期設定
+     */
+    protected function setInit() {
+        $this->values = [];
+        $this->record = null;
+        $this->isExecuted = false;
+    }
+}
---
+++ www/libs/php/imports/tsubasaLibs/type/ArrayLike.php
@@ -0,0 +1,135 @@
+<?php
+// -------------------------------------------------------------------------------------------------
+// 配列型クラス
+//
+// History:
+// 0.16.00 2024/03/23 作成。
+// -------------------------------------------------------------------------------------------------
+namespace tsubasaLibs\type;
+use ArrayAccess, Iterator, Countable;
+use Stringable;
+/**
+ * 配列型クラス
+ * 
+ * @since 0.16.00
+ * @version 0.16.00
+ */
+class ArrayLike implements ArrayAccess, Iterator, Countable {
+    // ---------------------------------------------------------------------------------------------
+    // プロパティ
+    /** @var array<int|string, mixed> 実データ */
+    protected $datas;
+    /** @var int 読み取り位置 */
+    protected $position;
+    // ---------------------------------------------------------------------------------------------
+    // コンストラクタ/デストラクタ
+    /**
+     * @param mixed ...$datas レコード
+     */
+    public function __construct(mixed ...$datas) {
+        $this->datas = $datas;
+    }
+    // ---------------------------------------------------------------------------------------------
+    // マジックメソッド
+    public function __clone() {}
+    public function __debugInfo() {
+        return $this->datas;
+    }
+    // ---------------------------------------------------------------------------------------------
+    // メソッド(オーバーライド、ArrayAccess)
+    /**
+     * 存在チェック
+     * 
+     * @param int|string|Stringable $offset キー値
+     * @return bool 結果
+     */
+    public function offsetExists(mixed $offset): bool {
+        if ($offset instanceof Stringable)
+            $offset = (string)$offset;
+        return isset($this->datas[$offset]);
+    }
+    /**
+     * 取得
+     * 
+     * @param int|string|Stringable $offset キー値
+     * @return mixed データ値
+     */
+    public function offsetGet(mixed $offset): mixed {
+        if ($offset instanceof Stringable)
+            $offset = (string)$offset;
+        return $this->datas[$offset];
+    }
+    /**
+     * 設定
+     * 
+     * @param ?int|string|Stringable $offset キー値
+     * @param mixed データ値
+     */
+    public function offsetSet(mixed $offset, mixed $value): void {
+        if ($offset === null) {
+            $this->datas[] = $value;
+        } else {
+            if ($offset instanceof Stringable)
+                $offset = (string)$offset;
+            $this->datas[$offset] = $value;
+        }
+    }
+    /**
+     * 破棄
+     * 
+     * @param ?int|string|Stringable $offset キー値
+     */
+    public function offsetUnset(mixed $offset): void {
+        if ($offset instanceof Stringable)
+            $offset = (string)$offset;
+        unset($this->datas[$offset]);
+    }
+    // ---------------------------------------------------------------------------------------------
+    // メソッド(オーバーライド、Iterator)
+    /**
+     * 読み取り位置を最初へ移動
+     */
+    public function rewind(): void {
+        $this->position = 0;
+    }
+    /**
+     * 読み取り位置を次へ移動
+     */
+    public function next(): void {
+        $this->position++;
+    }
+    /**
+     * 読み取り位置を取得
+     */
+    public function key(): mixed {
+        return $this->position;
+    }
+    /**
+     * 現在の読み取り位置のデータ値を取得
+     * 
+     * @return mixed データ値
+     */
+    public function current(): mixed {
+        return $this->datas[
+            array_keys($this->datas)[$this->position]
+        ];
+    }
+    /**
+     * 読み取り位置にデータがあるかどうか
+     * 
+     * @return bool 結果
+     */
+    public function valid(): bool {
+        return $this->position < count($this->datas);
+    }
+    // ---------------------------------------------------------------------------------------------
+    // メソッド(オーバーライド、Countable)
+    /**
+     * データ数を取得
+     * 
+     * @return int データ数
+     */
+    public function count(): int {
+        return count($this->datas);
+    }
+}

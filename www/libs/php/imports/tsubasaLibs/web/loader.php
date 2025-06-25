71be6dcfd0e306619e9534bb23837dc7848fe7be
Tsubasa Kadowaki <mocchihouse_mocchi@hotmail.co.jp>
2025-06-25 14:01:44 +0900

    (2024/06/26 14:17:18)ローダを各サブライブラリへ分割。

---
+++ www/libs/php/imports/tsubasaLibs/web/loader.php
@@ -0,0 +1,15 @@
+<?php
+// -------------------------------------------------------------------------------------------------
+// ライブラリローダ(tsubasaLibs\web)
+//
+// Use Libraries:
+// tsubasaLibs\type
+// tsubasaLibs\database
+// Smarty
+//
+// History:
+// 0.28.01 2024/06/26 作成。
+// -------------------------------------------------------------------------------------------------
+require_once __DIR__ . '/Events.php';
+require_once __DIR__ . '/SmartyPlugins.php';
+require_once __DIR__ . '/Curl.php';

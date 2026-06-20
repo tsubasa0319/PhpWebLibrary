8daf38a725100c7e8c18e337d0169695f5acaec3
Tsubasa Kadowaki <mocchihouse_mocchi@hotmail.co.jp>
2026-06-20 18:52:00 +0900

    (2025/02/19 15:18:52)Smartyのテンプレート/プラグインを www/smarty から tsubasaLibs/smarty へ移設し、ライブラリへ内包。 web/Smarty.php: setTemplateDir/addTemplateDir/setCompileDir/setCacheDir/addPluginsDirを追加。

--- www/libs/php/imports/tsubasaLibs/smarty/plugins/function.attributes.php
+++ www/libs/php/imports/tsubasaLibs/smarty/plugins/function.attributes.php
@@ -3,10 +3,11 @@
 // 属性文字列を取得
 //
 // History:
-// 0.015.00 2024/02/27 Tsubasa Kadowaki 作成。
+// 0.08.00 2024/02/27 作成。
+// 0.75.00 2025/02/19 ライブラリへ移動。
 // -------------------------------------------------------------------------------------------------
-require_once 'base/web/SmartyPlugins.php';
-use base\web\SmartyPlugins;
+require_once __DIR__ . '/../../web/SmartyPlugins.php';
+use tsubasaLibs\web\SmartyPlugins;
 
 function smarty_function_attributes($params, &$smarty) {
     return SmartyPlugins::attributes($params, $smarty);

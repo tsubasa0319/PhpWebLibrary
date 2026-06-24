5ea025614aae865a483562444a7ea51d8cfb589f
Tsubasa Kadowaki <mocchihouse_mocchi@hotmail.co.jp>
2026-06-24 10:53:54 +0900

    (2024/09/26 18:21:09)循環参照対策のため、子クラスのインスタンスを弱い参照でプロパティに持つように変更。 database/DbBase.php: テーブルインスタンスリストを弱い参照リストに変更。ステートメントクラス属性のパラメータが循環参照のため、弱い参照へ変更。デストラクタを追加。 database/DbStatement.php: コンストラクタのパラメータを弱い参照に変更。 database/Record.php: コンストラクタの受取パラメータを弱い参照に変更。プロパティをTableからTableStatementに変更。 database/TableStatement.php: フェッチモード属性のパラメータが循環参照のため、弱い参照へ変更。 database/Table.php: テンポラリテーブルリストを弱い参照リストに変更。デストラクタを追加。 database/Indexes.php: テーブルインスタンスとインデックスインスタンスリストを弱い参照に変更。 database/QueryPlanning.php: テーブルインスタンスを弱い参照へ変更。

--- www/libs/php/imports/tsubasaLibs/database/TableStatement.php
+++ www/libs/php/imports/tsubasaLibs/database/TableStatement.php
@@ -7,15 +7,17 @@
 // 0.10.00 2024/03/08 継承元がDB情報無しに対応したため、合わせる。
 // 0.22.00 2024/05/17 新規レコードを取得時、レコード(変更前)をnullへ変更。
 // 0.40.00 2024/09/25 フェッチモードの変更を、テーブルの設定時に行うように変更。メモリリーク対策のため。
+// 0.40.01 2024/09/26 フェッチモード属性のパラメータが循環参照のため、弱い参照へ変更。
 // -------------------------------------------------------------------------------------------------
 namespace tsubasaLibs\database;
 require_once __DIR__ . '/Record.php';
+use WeakReference;
 
 /**
  * テーブルステートメントクラス
  * 
  * @since 0.00.00
- * @version 0.40.00
+ * @version 0.40.01
  */
 class TableStatement extends DbStatement {
     // ---------------------------------------------------------------------------------------------
@@ -27,12 +29,6 @@ class TableStatement extends DbStatement {
 
     // ---------------------------------------------------------------------------------------------
     // コンストラクタ/デストラクタ
-    /**
-     * @param ?DbBase $db DBクラス
-     */
-    protected function __construct(?DbBase $db) {
-        parent::__construct($db);
-    }
 
     // ---------------------------------------------------------------------------------------------
     // マジックメソッド
@@ -54,7 +50,8 @@ class TableStatement extends DbStatement {
         // フェッチモードを変更、レコードインスタンスを返すようにする
         if (!$this->isTemp)
             $this->setFetchMode(
-                DbBase::FETCH_CLASS, $this->recordClass, [$table]
+                // 自身を循環参照するため、弱い参照
+                DbBase::FETCH_CLASS, $this->recordClass, [WeakReference::create($this)]
             );
     }
 
@@ -63,7 +60,7 @@ class TableStatement extends DbStatement {
      */
     public function getNewRecord() {
         /** @var Record */
-        $record = new $this->recordClass($this->table);
+        $record = new $this->recordClass(WeakReference::create($this));
         $record->setNothing();
         $record->previousRecord = null;
         return $record;

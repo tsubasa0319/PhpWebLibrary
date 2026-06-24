378ad876ba0bec356e4ce1f568421dc35ed1eabf
Tsubasa Kadowaki <mocchihouse_mocchi@hotmail.co.jp>
2026-06-24 10:54:00 +0900

    (2024/10/02 16:06:41)mail/AddressInfo.php: アドレスペアを生成メソッドを追加。

--- www/libs/php/imports/tsubasaLibs/mail/AddressInfo.php
+++ www/libs/php/imports/tsubasaLibs/mail/AddressInfo.php
@@ -4,6 +4,7 @@
 //
 // History:
 // 0.41.00 2024/10/02 作成。
+// 0.41.01 2024/10/02 アドレスペアを生成メソッドを追加。
 // -------------------------------------------------------------------------------------------------
 namespace tsubasaLibs\mail;
 
@@ -11,7 +12,7 @@ namespace tsubasaLibs\mail;
  * メールアドレス情報クラス
  * 
  * @since 0.41.00
- * @version 0.41.00
+ * @version 0.41.01
  */
 class AddressInfo {
     // ---------------------------------------------------------------------------------------------
@@ -45,6 +46,27 @@ class AddressInfo {
             mb_encode_mimeheader($this->name ?? '', $charset),
             $this->address ?? ''
         );
+    }
+
+    // ---------------------------------------------------------------------------------------------
+    // メソッド(静的)
+    /**
+     * アドレスペアを生成
+     * 
+     * 文字列:[name]<[mail]>から、配列:[[mail], [name]]を生成します。
+     * 
+     * @since 0.41.01
+     * @param string $addressWithName 名前付きメールアドレス
+     * @return string[] メールアドレスと名前の配列
+     */
+    static public function makeAddressPair(string $addressWithName): array {
+        $match = null;
+        if (!preg_match('/\A(.*)<([^<]*)>\z/', $addressWithName, $match))
+            return [$addressWithName];
+
+        $name = $match[1];
+        $address = $match[2];
+        return [$address, $name];
     }
 
     // ---------------------------------------------------------------------------------------------

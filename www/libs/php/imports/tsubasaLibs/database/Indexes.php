16c9a106e8fb3a118581d9a6080d3222aeae1219
Tsubasa Kadowaki <mocchihouse_mocchi@hotmail.co.jp>
2026-06-24 10:53:56 +0900

    (2024/09/27 14:22:18)生成済インスタンスは通常の参照へ戻す。途中でメモリ解放されてしまうため。メモリ解放はdisposeメソッドか、ガベージコレクションで行う。 database/DbBase.php: テーブルインスタンスリストを通常の参照リストに変更。disposeメソッドでテーブルインスタンスを明示的に廃棄処理。 database/Indexes.php: インデックスインスタンスリストを通常の参照リストに変更。disposeメソッドを追加。 database/Record.php: convertNameメソッドで、ステートメントインスタンスがNullである場合を考慮(null-safe演算子を使用)。 database/Table.php: テンポラリテーブルリストを通常の参照リストに変更。disposeメソッドを追加。__destructメソッドのデバッグ出力をstatic::classに変更。 cli/TaskImportCsv.php: 空行を追加。

--- www/libs/php/imports/tsubasaLibs/database/Indexes.php
+++ www/libs/php/imports/tsubasaLibs/database/Indexes.php
@@ -6,6 +6,7 @@
 // 0.00.00 2024/01/23 作成。
 // 0.40.01 2024/09/26 子クラスのインスタンスは、弱い参照でプロパティに持つように変更。
 //                    テーブルインスタンスを弱い参照へ変更。循環参照のため。
+// 0.40.02 2024/09/27 生成済インデックスインスタンスは通常の参照へ戻す。途中でメモリ解放されてしまうため。
 // -------------------------------------------------------------------------------------------------
 namespace tsubasaLibs\database;
 use WeakReference;
@@ -14,15 +15,15 @@ use WeakReference;
  * インデックスリストクラス
  * 
  * @since 0.00.00
- * @version 0.40.01
+ * @version 0.40.02
  */
 class Indexes {
     // ---------------------------------------------------------------------------------------------
     // プロパティ
     /** @var WeakReference<Table> テーブルインスタンスの参照 */
     protected $tableRef;
-    /** @var WeakReference<Table>[] 生成したインデックスインスタンスの参照リスト */
-    protected $indexInstanceRefs;
+    /** @var Table[] 生成済インデックスインスタンスのリスト */
+    protected $indexInstances;
 
     // ---------------------------------------------------------------------------------------------
     // コンストラクタ/デストラクタ
@@ -35,12 +36,33 @@ class Indexes {
     }
 
     // ---------------------------------------------------------------------------------------------
+    // メソッド
+    /**
+     * 廃棄処理
+     * 
+     * 直接には実行せずに、DBインスタンスより実行してください。
+     * 
+     * @since 0.40.02
+     */
+    public function dispose() {
+        // 参照を外す
+        if ($this->indexInstances !== null) {
+            $indexInstances = $this->indexInstances;
+            $this->indexInstances = null;
+
+            // 参照先を廃棄処理
+            foreach ($indexInstances as $indexInstance)
+                $indexInstance->dispose();
+        }
+    }
+
+    // ---------------------------------------------------------------------------------------------
     // 内部処理
     /**
      * 初期設定
      */
     protected function setInit() {
-        $this->indexInstanceRefs = [];
+        $this->indexInstances = [];
     }
 
     /**
@@ -58,14 +80,12 @@ class Indexes {
             throw new DbException('Table is already closed');
 
         // 再利用
-        foreach ($this->indexInstanceRefs as $indexInstanceRef) {
-            $indexInstance = $indexInstanceRef->get();
+        foreach ($this->indexInstances as $indexInstance)
             if ($indexInstance instanceof $indexClass) return $indexInstance;
-        }
 
         // 生成し、キャッシュを取る
         $indexInstance = new $indexClass($table->db);
-        $this->indexInstanceRefs[] = WeakReference::create($indexInstance);
+        $this->indexInstances[] = $indexInstance;
         return $indexInstance;
     }
 }

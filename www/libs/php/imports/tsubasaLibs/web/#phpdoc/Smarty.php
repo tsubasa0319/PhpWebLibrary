589ef979dff76bada2efb10501887f8c54d89c97
Tsubasa Kadowaki <mocchihouse_mocchi@hotmail.co.jp>
2026-06-25 15:48:42 +0900

    (2025/02/19 15:18:52)Smartyのテンプレート/プラグインを www/smarty から tsubasaLibs/smarty へ移設し、ライブラリへ内包。 web/Smarty.php: setTemplateDir/addTemplateDir/setCompileDir/setCacheDir/addPluginsDirを追加。

--- www/libs/php/imports/tsubasaLibs/web/#phpdoc/Smarty.php
+++ www/libs/php/imports/tsubasaLibs/web/#phpdoc/Smarty.php
@@ -4,6 +4,7 @@
 //
 // History:
 // 0.08.00 2024/02/27 作成。
+// 0.75.00 2025/02/19 setTemplateDir/addTemplateDir/setCompileDir/setCacheDir/addPluginsDirを追加。
 // -------------------------------------------------------------------------------------------------
 use tsubasaLibs\web\WebException;
 
@@ -12,5 +13,60 @@ class Smarty {
     // コンストラクタ/デストラクタ
     public function __construct() {
         throw new WebException('Smarty hasn\'t been installed');
+    }
+
+    // ---------------------------------------------------------------------------------------------
+    // メソッド
+    /**
+     * テンプレートディレクトリを登録
+     * 
+     * @param string|string[] $template_dir テンプレートディレクトリ
+     * @param bool $isConfig
+     * @return static チェーン用
+     */
+    public function setTemplateDir($template_dir, $isConfig = false) {
+        return $this;
+    }
+
+    /**
+     * テンプレートディレクトリを追加
+     * 
+     * @param string|string[] $template_dir テンプレートディレクトリ
+     * @param ?string $key
+     * @param bool $isConfig
+     * @return static チェーン用
+     */
+    public function addTemplateDir($template_dir, $key = null, $isConfig = false) {
+        return $this;
+    }
+
+    /**
+     * コンパイルディレクトリを登録
+     * 
+     * @param string $compile_dir コンパイルディレクトリ
+     * @return static チェーン用
+     */
+    public function setCompileDir($compile_dir) {
+        return $this;
+    }
+
+    /**
+     * キャッシュディレクトリを登録
+     * 
+     * @param string $cache_dir キャッシュディレクトリ
+     * @return static チェーン用
+     */
+    public function setCacheDir($cache_dir) {
+        return $this;
+    }
+
+    /**
+     * プラグインディレクトリを追加
+     * 
+     * @param ?string|string[] $plugins_dir プラグインディレクトリ
+     * @return static チェーン用
+     */
+    public function addPluginsDir($plugins_dir) {
+        return $this;
     }
 }
